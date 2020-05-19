23d1fccdf0996bab28ab6a4af0037fd249759267
Cassis <cassismjones@gmail.com>
2020-05-19 15:03:45 +0100

    Section 7: Connecting files to database

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'HTTPS://rubygems.org'
 gem 'sinatra'
 gem 'rspec'
 gem 'capybara'
+gem 'pg'
 
 group :development, :test do
   gem "rubocop", "0.79.0"
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,10 @@
+require 'PG'
+
 class Bookmark
   attr_reader :library
   def initialize
-    @library = [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-     ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks;')
+    @library = result.map { |bookmark| bookmark['url'] } 
    end
  end
