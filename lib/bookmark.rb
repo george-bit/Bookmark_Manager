47d6cff4bfc1215451fd69b29a6248989e80c2de
George Holborn <georgeholborn5@gmail.com>
2020-05-21 14:57:29 +0100

    Bookmark spec and class to include an initialize and instance v

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,7 +1,13 @@
 require 'PG'
 
 class Bookmark
-  attr_reader :library
+  attr_reader :id, :title, :url
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end
 
   def self.all
     if ENV['RACK_ENV'] == 'test'
@@ -11,8 +17,10 @@ class Bookmark
     end
 
     result = connection.exec('SELECT * FROM bookmarks;')
-    result.map { |bookmark| bookmark['url'] }
-   end
+    result.map do |bookmark|
+      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    end
+  end
 
   def self.create(url:, title:)
     if ENV['RACK_ENV'] == 'test'
@@ -23,4 +31,4 @@ class Bookmark
 
     connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, url, title")
   end
-end
+  end
